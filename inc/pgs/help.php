669f2e24fdbe28409240ab4538d7c343e9948b7d
JaydenLeHiep <98736015+JaydenLeHiep@users.noreply.github.com>
2023-04-20 22:06:54 +0200

    new help page

---
+++ inc/pgs/help.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Kontakt</title>
+
+    <?php
+    include '../includes/head.php';
+    ?>
+
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <h1 class="display-1">Hilfe</h1>
+                <p class="lead text-muted">Here you can get help if you are lost on our website!</p>
+            </div>
+        </div>
+    </section>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-12">
+                <h2>Hilfe für Kunden</h2>
+                <p>Willkommen bei unserem Webshop! Wir möchten sicherstellen, dass Sie eine großartige Erfahrung machen, während Sie auf unserer Website einkaufen. Hier sind einige Tipps, die Ihnen dabei helfen können:</p>
+                <ul>
+                    <li>Verwenden Sie die Navigation oben auf der Seite, um schnell zu der Kategorie zu gelangen, die Sie interessiert.</li>
+                    <li>Um ein Zimmer zu buchen, klicken Sie auf "Zimmerreservierung".</li>
+                    <li>Wenn Sie neu sind, registrieren Sie sich, indem Sie auf "Registrieren" klicken. Wenn Sie bereits ein Konto haben, können Sie sich in der rechten oberen Ecke ab- oder anmelden.</li>
+                    <li>Unser Kundensupport-Team ist immer bereit, Ihnen zu helfen. Wenn Sie Fragen oder Probleme haben, zögern Sie nicht, uns zu kontaktieren.</li>
+                </ul>
+            </div>
+            <div class="col-lg-6 col-12">
+                <h2>Häufig gestellte Fragen</h2>
+                <ul>
+                    <li><b>Wie kann ich meine Bestellung stornieren?</b><br> Sie können Ihre Bestellung stornieren, indem Sie sich an unser Kundensupport-Team wenden.</li>
+                    <li><b>Wie kann ich meine Zahlungsmethode ändern?</b><br> Sie können Ihre Zahlungsmethode ändern, indem Sie in Ihrem Konto auf "Zahlungsmethoden" klicken.</li>
+                    <li><b>Wie kann ich meine Lieferadresse ändern?</b><br> Sie können Ihre Lieferadresse ändern, indem Sie in Ihrem Konto auf "Adressen" klicken.</li>
+                    <li><b>Wie kann ich meine Bestellung verfolgen?</b><br> Sie können Ihre Bestellung verfolgen, indem Sie auf den Link in der Bestellbestätigungs-E-Mail klicken, die wir Ihnen gesendet haben.</li>
+                    <li><b>Wie sicher sind meine Daten?</b><br> Wir legen großen Wert auf den Schutz Ihrer Daten. Alle Ihre Informationen werden sicher und vertraulich behandelt.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
