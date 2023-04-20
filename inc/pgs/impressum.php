a1fb27e47fe28521f1d33cb5af9910ec07f6f576
daniel <daniel.pfeifhofer.98@gmail.com>
2023-04-20 22:29:31 +0200

    Implement Product pages

--- inc/pgs/impressum.php
+++ inc/pgs/impressum.php
@@ -1,32 +1,80 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-
-  <title>Document</title>
-
-  <?php 
+  <head>
+    <title>Login</title>
+        
+    <?php
         include '../includes/head.php';
-    ?>
+    ?> 
+  </head>
+  <body class="d-flex flex-column min-vh-100 ">
+    <div class="container site-font-color">
+      <div class="card card-bg shadow-2-strong card-registration mt-3 p-2 flex-column " style="border-radius: 15px; max-width: 800px;">
+        <div>
+          <h1 class="ms-3 my-3">Impressum</h1>
+        </div>
+        
+        <ul class="list-group list-group-flush ">
+        <li class="list-group-item bg-transparent site-font-color"><h6><b>Webshop Kasallek</b></h6></li>
+          <li class="list-group-item bg-transparent site-font-color">Gesellschaft mit beschränkter Haftung</li>
+          <li class="list-group-item bg-transparent site-font-color">Mariahilferstrasse 212, 1140 Wien</li>
+          <li class="list-group-item bg-transparent site-font-color">Tel: +43 1 345 67 89</li>
+          <li class="list-group-item bg-transparent site-font-color">Fax +41 1 345 67 89 99</li>
+          <li class="list-group-item bg-transparent site-font-color"><a class="site-font-color" href="mailto:info@hotel-springer.de">info@hotel-springer.de</a></li>
+          <li class="list-group-item bg-transparent site-font-color"></li>
+        </ul>
 
-</head>
-<body>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item bg-transparent site-font-color"></li>
+          <li class="list-group-item bg-transparent site-font-color">UID-Nr: ATU12345678</li>
+          <li class="list-group-item bg-transparent site-font-color">FN: 123456a</li>
+          <li class="list-group-item bg-transparent site-font-color">FB-Gericht: Gerichtsstand Wien</li>
+          <li class="list-group-item bg-transparent site-font-color">Mitglied von ÖHV und WKO</li>
+          <li class="list-group-item bg-transparent site-font-color"></li>
+        </ul>
 
-<h1>EMPTY SPACE </h1>
-<h1>EMPTY SPACE </h1>
-<h1>EMPTY SPACE </h1>
-<h1>EMPTY SPACE </h1>
-<h1>EMPTY SPACE </h1>
-<h1>EMPTY SPACE </h1>
-<h1>EMPTY SPACE </h1>
-<h1>EMPTY SPACE </h1>
-<h1>EMPTY SPACE </h1>
-<h1>EMPTY SPACE </h1>
-<h1>EMPTY SPACE </h1>
-  
+        <ul class="list-group list-group-flush ">
+          <li class="list-group-item bg-transparent site-font-color"></li>
+          <li class="list-group-item bg-transparent ms-0 site-font-color"><h6>Berufsrecht:</h6></li>
+          <li class="list-group-item bg-transparent site-font-color">Gewerbeordnung: <a class="site-font-color" href="www.ris.bka.gv.at">www.ris.bka.gv.at</a></li>
+          <li class="list-group-item bg-transparent site-font-color">Bezirkshaupmannschaft Wien</li>
+          <li class="list-group-item bg-transparent site-font-color">Verbraucher haben die Möglichkeit, Beschwerden an die Online-Streitbeilegungsplattform der EU zu richten: <a class="site-font-color" href="http://ec.europa.eu/odr">http://ec.europa.eu/odr</a>.</li>
+          <li class="list-group-item bg-transparent site-font-color">Sie können allfällige Beschwerde auch am die oben angegebene E-Mail-Adresse richten.</li>
+          <li class="list-group-item bg-transparent site-font-color"></li>
+        </ul>
+    </div>
 
-<?php 
-include '../includes/footer.php';
-    ?>
+    <div class=" container my-4 ">
+      <div class="row mx-1">
+        <h3 class="site-font-color">Shopleitung</h3>
+        <div class="col-9 col-md-3 img-fluid">
+            <img class="img-fluid" width="200" src="./../..\res\img/impressum.jpg">
+            <figcaption class="figure-caption site-font-color fw-bold">Daniel</figcaption>
+        </div>
+        <div class="col-9 col-md-3 img-fluid ">
+            <img class="img-fluid" width="200" src="./../..\res\img\impressum.jpg">
+            <figcaption class="figure-caption site-font-color fw-bold">Rafael</figcaption>
+        </div>
+        <div class="col-9 col-md-3 img-fluid ">
+            <img class="img-fluid" width="200" src="./../..\res\img/impressum.jpg">
+            <figcaption class="figure-caption site-font-color fw-bold">Elias</figcaption>
+        </div>
+        <div class="col-9 col-md-3 img-fluid ">
+            <img class="img-fluid" width="200" src="./../..\res\img\impressum.jpg">
+            <figcaption class="figure-caption site-font-color fw-bold">Hiep</figcaption>
+        </div>
+        <div class="col-9 col-md-3 img-fluid ">
+            <img class="img-fluid" width="200" src="./../..\res\img\impressum.jpg">
+            <figcaption class="figure-caption site-font-color fw-bold">Duong</figcaption>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <?php
+  include '../includes/footer.php';
+  ?>
 
 </body>
 </html>
