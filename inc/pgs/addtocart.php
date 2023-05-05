811ab6dd284a57a71b1e3a4f1f61adda067099ab
Rafael Jeitler <116429608+Naxnor@users.noreply.github.com>
2023-05-05 16:49:53 +0200

    shoppingcart  added not final

---
+++ inc/pgs/addtocart.php
@@ -0,0 +1,44 @@
+<?php
+
+// Connect to the database
+include '../includes/head.php';
+require_once('../../config/dbaccess.php');
+
+
+$connection = new mysqli($host, $user, $password, $database);
+
+// Check for errors
+if (mysqli_connect_errno()) {
+  die("Database connection failed: " .
+    mysqli_connect_error() .
+    " (" . mysqli_connect_errno() . ")"
+  );
+}
+
+// Check if a product has been added to the cart
+if (isset($_POST["product_id"]) && isset($_POST["quantity"])) {
+  // Check if the user is logged in
+  if (!isset($_SESSION["user_id"])) {
+    // If the user is not logged in, redirect to the login page
+   
+    exit();
+  }
+
+  $user_id = $_SESSION["user_id"];
+  $product_id = intval($_POST["product_id"]);
+  $quantity = intval($_POST["quantity"]);
+
+  // Insert the product into the shopping cart table
+  $query = "INSERT INTO shoppingcart (user_id, product_id, quantity) VALUES ('$user_id', '$product_id', '$quantity')";
+  var_dump($_SESSION);
+  
+  if (mysqli_query($connection, $query)) {
+    // Redirect back to the previous page
+    
+    exit();
+  } else {
+    // Display an error message
+    echo "Error: " . mysqli_error($connection);
+  }
+}
+?>
