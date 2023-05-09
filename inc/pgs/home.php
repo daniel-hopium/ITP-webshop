5021b217762c2d568baf4706ad532da2e9508f5f
daniel <daniel.pfeifhofer.98@gmail.com>
2023-05-09 12:04:00 +0200

    Added new homepage

--- inc/pgs/home.php
+++ inc/pgs/home.php
@@ -11,68 +11,152 @@
 </head>
 
 <body class="d-flex flex-column min-vh-100">
-    <div class="container-fluid p-0">
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active border border-dark">
-                    <img src=".\..\..\res\img/inprogress.jpg" class="d-block w-100 " alt="...">
-                    <div class="carousel-caption d-none d-md-block ">
-                        <h4></h4>
+    <div class="container">
+        <h2 class="h2 mt-4">RABATTAKTIONEN</h2>
+        <div class="row ">
+            <div class="col-md-6">
+                <div id="carousel1" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="../../res/img/sale2.jpg" class="d-block w-100" alt="Image 1">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../../res/img/sale2.jpg" class="d-block w-100" alt="Image 2">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../../res/img/sale2.jpg" class="d-block w-100" alt="Image 3">
+                        </div>
                     </div>
-                </div>
-                <div class="carousel-item">
-                    <img src=".\..\..\res\img/inprogress.jpg" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h4 class=""></h4>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src=".\..\..\res\img/inprogress.jpg" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h4></h4>
-                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel1"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carousel1"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-    </div>
-
-    <div class="container-fluid ">
-        <div class="container card text-white shadow bg-brown my-5  ">
-            <div class="card-body<">
-                <h1 class="h1 text-center  ">NEUE FERNSEHER IM ANGEBOT</h1>
-                <img class="img-fluid rounded" alt="Image" src=".\..\..\res\img\new_product_01.jpg">
-                <div class="my-2 fs-5">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt nobis aspernatur animi,
-                        distinctio architecto quam accusantium maxime asperiores suscipit mollitia dolor molestias,
-                        explicabo necessitatibus nemo cum similique nam perferendis iure.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum soluta dolorum animi qui deserunt
-                        beatae odio consectetur saepe natus praesentium optio, blanditiis cumque unde eos ratione? Velit
-                        eaque repudiandae inventore.</p>
-                    <p>ONLY 5999 BUCKS </p>
+            <div class="col-md-6">
+                <div id="carousel2" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="../../res/img/sale3.jpg" class="d-block w-100" alt="Image 4">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../../res/img/sale3.jpg" class="d-block w-100" alt="Image 5">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../../res/img/sale3.jpg" class="d-block w-100" alt="Image 6">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel2"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carousel2"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </div>
         </div>
-    </div>
 
-    <?php
+
+        <h2 class="h2 mt-4">NEUANKÖMMLINGE</h2>
+        <div class="col-md-12 ">
+            <div id="carousel1" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../../res/img/inprogress.jpg" class="d-block w-100" alt="Image 1">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../../res/img/inprogress.jpg" class="d-block w-100" alt="Image 2">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../../res/img/inprogress.jpg" class="d-block w-100" alt="Image 3">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carousel1" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carousel1" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+
+            <h2 class="h2 mt-4">EMFEHLUNGEN FÜR SIE</h2>
+            <div id="myCarousel" class="carousel slide my-4" data-bs-ride="carousel">
+                <!-- Wrapper for carousel items -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 1">
+                            </div>
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 2">
+                            </div>
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 3">
+                            </div>
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 4">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 1">
+                            </div>
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 2">
+                            </div>
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 3">
+                            </div>
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 4">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 1">
+                            </div>
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 2">
+                            </div>
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 3">
+                            </div>
+                            <div class="col-md-3">
+                                <img src="../../res/img/new_product_01.jpg" class="img-fluid" alt="Image 4">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Left and right controls -->
+                <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only"></span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only"></span>
+                </button>
+            </div>
+        </div>
+        <?php
  include '../includes/footer.php';
     ?>
 
