c2fd20999fa4c14c5f3346286a89e30836cfe3d7
daniel <daniel.pfeifhofer.98@gmail.com>
2023-05-23 14:12:38 +0200

    Shopping Cart + Navbar Fix

---
+++ config/check_shoppingcart_amount.php
@@ -0,0 +1,17 @@
+<?php
+// Checks if admin so navbar enables admin only tabs
+require_once('dbaccess.php'); //to retrieve connection details
+
+$db_obj = new mysqli($host, $user, $password, $database);
+
+if ($db_obj->connect_error) {
+    echo "Connection Error: " . $db_obj->connect_error;
+    exit();
+}
+
+// Filters out role of current user
+// if(isset($_SESSION['username']))
+
+$currentShoppingcartAmount = mysqli_query($db_obj, "SELECT * FROM shoppingcart");
+$currentShoppingcartAmount = mysqli_num_rows($currentShoppingcartAmount);
+
