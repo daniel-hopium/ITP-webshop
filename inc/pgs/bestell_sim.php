13c19daba3db34c4b1f3ffcb06f3824e9b427d51
Rafael Jeitler <116429608+Naxnor@users.noreply.github.com>
2023-06-14 14:45:43 +0200

    Updated
    Updated Bestell SIm so we group Orders

--- inc/pgs/bestell_sim.php
+++ inc/pgs/bestell_sim.php
@@ -14,24 +14,62 @@ $connection = new mysqli($host, $user, $password, $database);
 // Retrieve the user_id from the POST parameters
 $user_id = $_POST["user_id"];
 
+// Generate a unique order ID
+$orderid = generateOrderID($connection);
+
 // Retrieve the items in the cart from the database
 $query = "SELECT shoppingcart.quantity, products.id, products.name, products.price FROM shoppingcart INNER JOIN products ON shoppingcart.product_id = products.id WHERE shoppingcart.user_id = $user_id";
 $result = mysqli_query($connection, $query);
 
 // Loop through the items in the cart
 while ($row = mysqli_fetch_assoc($result)) {
-    // Calculate the total price for THIS product
-    $total_price = $row["price"] * $row["quantity"];
+  // Calculate the total price for THIS product
+  $total_price = $row["price"] * $row["quantity"];
 
-    // Insert the order into the new_orders table
-    $query = "INSERT INTO new_orders (buyer_name, buyer_email, product_id, quantity, total_price, status) VALUES (?, ?, ?, ?, ?, 'pending')";
-    $stmt = $connection->prepare($query);
-    $stmt->bind_param("ssiii", $user_id, $_SESSION["username"], $row["id"], $row["quantity"], $total_price); // Assuming $user_id is used as both buyer_name and buyer_email for testing purposes.
-    $stmt->execute();
+  // Insert the order into the new_orders table
+  $query = "INSERT INTO new_orders (orderid, buyer_name, buyer_email, product_id, quantity, total_price, status, order_date) VALUES (?, ?, ?, ?, ?, ?, 'pending', NOW())";
+  $stmt = $connection->prepare($query);
+  $stmt->bind_param("ssiiis", $orderid, $user_id, $_SESSION["username"], $row["id"], $row["quantity"], $total_price);
+  $stmt->execute();
 }
+
+// Drop the contents of the shopping cart
+$deleteQuery = "DELETE FROM shoppingcart WHERE user_id = $user_id";
+mysqli_query($connection, $deleteQuery);
+
 
 // Close the database connection
 mysqli_close($connection);
+
+// Function to generate a unique order ID
+function generateOrderID($connection) {
+  $orderid = generateRandomID();
+
+  // Check if the generated order ID already exists in the database
+  $query = "SELECT COUNT(*) AS count FROM new_orders WHERE orderid = ?";
+  $stmt = $connection->prepare($query);
+  $stmt->bind_param("s", $orderid);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $row = $result->fetch_assoc();
+  $count = $row["count"];
+
+  // If the order ID already exists, generate a new one recursively
+  if ($count > 0) {
+      return generateOrderID($connection); // Generate a new order ID
+  }
+
+  return $orderid;
+}
+
+// Function to generate a random order ID
+function generateRandomID() {
+  $unique_id = uniqid('', true);
+  $random_bytes = random_bytes(8);
+  $random_number = hexdec(bin2hex($random_bytes));
+  $order_id = $unique_id . $random_number;
+  return $order_id;
+}
 ?>
 
 <div id="timer">5</div>
@@ -50,4 +88,3 @@ mysqli_close($connection);
     }
   }, 1000);
 </script>
-
