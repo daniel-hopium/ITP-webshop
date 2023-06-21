24adcb81a49c18aff1c3e2ffe2adb689848bd9c6
JaydenLeHiep <98736015+JaydenLeHiep@users.noreply.github.com>
2023-06-21 19:46:16 +0200

    updated help

--- inc/pgs/help.php
+++ inc/pgs/help.php
@@ -14,8 +14,8 @@
     <section class="py-5 text-center container">
         <div class="row py-lg-5">
             <div class="col-lg-6 col-md-8 mx-auto">
-                <h1 class="display-1">Hilfe</h1>
-                <p class="lead text-muted">Here you can get help if you are lost on our website!</p>
+                <h1 class="display-1">Willkommen auf der Hilfeseite für unseren Webshop!</h1>
+                <p class="lead text-muted">Hier findest du Antworten auf häufig gestellte Fragen (Q&A), um dir bei der Nutzung unseres Webshops zu helfen. Solltest du weitere Fragen haben, zögere nicht, uns zu kontaktieren.</p>
             </div>
         </div>
     </section>
@@ -23,37 +23,36 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 col-12">
-                <h2>Hilfe für Kunden</h2>
-                <p>Willkommen bei unserem Webshop! Wir möchten sicherstellen, dass Sie eine großartige Erfahrung machen,
-                    während Sie auf unserer Website einkaufen. Hier sind einige Tipps, die Ihnen dabei helfen können:
-                </p>
-                <ul>
-                    <li>Verwenden Sie die Navigation oben auf der Seite, um schnell zu der Kategorie zu gelangen, die
-                        Sie interessiert.</li>
-                    <li>Wenn Sie neu sind, registrieren Sie sich, indem Sie auf "Registrieren" klicken. Wenn Sie bereits
-                        ein Konto haben, können Sie sich in der rechten oberen Ecke ab- oder anmelden.</li>
-                    <li>Unser Kundensupport-Team ist immer bereit, Ihnen zu helfen. Wenn Sie Fragen oder Probleme haben,
-                        zögern Sie nicht, uns zu kontaktieren.</li>
-                </ul>
+                <h2>Q&A - Häufig gestellte Fragen</h2>
+                <ol>
+                    <li><b>Wie kann ich einen Artikel bestellen?</b><br> Um einen Artikel zu bestellen, folge bitte diesen Schritten:
+                        <ol>
+                            <li>Suche den gewünschten Artikel über die Suchleiste oder die Kategorien.</li>
+                            <li>Klicke auf den Artikel, um weitere Informationen zu erhalten.</li>
+                            <li>Wähle die gewünschte Variante (falls zutreffend) und die gewünschte Anzahl aus.</li>
+                            <li>Klicke auf "In den Warenkorb" oder "Jetzt kaufen".</li>
+                            <li>Gehe zum Warenkorb und überprüfe deine Bestellung.</li>
+                            <li>Klicke auf "Zur Kasse gehen" und folge den Anweisungen zur Zahlung.</li>
+                        </ol>
+                    </li>
+                    <li><b>Welche Zahlungsmethoden werden akzeptiert?</b><br> Wir akzeptieren verschiedene Zahlungsmethoden, darunter Kreditkarten (Visa, Mastercard, American Express), PayPal und Banküberweisungen. Wähle einfach die bevorzugte Zahlungsmethode während des Bestellvorgangs aus.</li>
+                    <li><b>Wie lange dauert der Versand?</b><br> Die Versanddauer hängt von deinem Standort und der gewählten Versandmethode ab. Normalerweise beträgt die Lieferzeit innerhalb des Landes X Tage und international X bis X Tage. Bitte beachte, dass es bei internationalen Sendungen zu Verzögerungen aufgrund von Zollabfertigung kommen kann.</li>
+                </ol>
             </div>
             <div class="col-lg-6 col-12">
-                <h2>Häufig gestellte Fragen</h2>
-                <ul>
-                    <li><b>Wie kann ich meine Bestellung stornieren?</b><br> Sie können Ihre Bestellung stornieren,
-                        indem Sie sich an unser Kundensupport-Team wenden.</li>
-                    <li><b>Wie kann ich meine Zahlungsmethode ändern?</b><br> Sie können Ihre Zahlungsmethode ändern,
-                        indem Sie in Ihrem Konto auf "Zahlungsmethoden" klicken.</li>
-                    <li><b>Wie kann ich meine Lieferadresse ändern?</b><br> Sie können Ihre Lieferadresse ändern, indem
-                        Sie in Ihrem Konto auf "Adressen" klicken.</li>
-                    <li><b>Wie kann ich meine Bestellung verfolgen?</b><br> Sie können Ihre Bestellung verfolgen, indem
-                        Sie auf den Link in der Bestellbestätigungs-E-Mail klicken, die wir Ihnen gesendet haben.</li>
-                    <li><b>Wie sicher sind meine Daten?</b><br> Wir legen großen Wert auf den Schutz Ihrer Daten. Alle
-                        Ihre Informationen werden sicher und vertraulich behandelt.</li>
-                </ul>
+                <h2>&nbsp;</h2>
+                <ol start="4">
+                    <li><b>Kann ich meine Bestellung stornieren oder ändern?</b><br> Eine Stornierung oder Änderung einer Bestellung ist möglich, solange sie noch nicht versandt wurde. Kontaktiere umgehend unseren Kundendienst und gib deine Bestellnummer an, um Unterstützung zu erhalten.</li>
+                    <li><b>Wie kann ich den Status meiner Bestellung überprüfen?</b><br> Sobald deine Bestellung versandt wurde, erhältst du eine Versandbestätigung per E-Mail mit einem Tracking-Link. Mit diesem Link kannst du den aktuellen Status deiner Bestellung verfolgen. Alternativ kannst du dich auch in deinem Benutzerkonto anmelden und den Bestellverlauf einsehen.</li>
+                    <li><b>Was ist die Rückgaberegelung?</b><br> Wir bieten eine Rückgaberegelung an. Solltest du mit deinem Artikel nicht zufrieden sein, kannst du ihn innerhalb von X Tagen nach Erhalt zurücksenden. Bitte beachte, dass bestimmte Artikel von der Rückgabe ausgeschlossen sein können. Weitere Informationen findest du in unserer Rückgaberichtlinie.</li>
+                    <li><b>Wie kontaktiere ich den Kundendienst?</b><br> Du kannst unseren Kundendienst per E-Mail unter support@example.com oder telefonisch unter der Nummer XXX-XXXXX erreichen. Unser Team steht dir gerne zur Verfügung und wird deine Anfrage so schnell wie möglich bearbeiten.</li>
+                </ol>
             </div>
         </div>
     </div>
 </body>
+
+
 
 <?php
 include '../includes/footer.php';
