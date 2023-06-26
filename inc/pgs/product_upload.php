f2952edb8cc60260ed71f4dc0e9c29a009dae0a9
daniel <daniel.pfeifhofer.98@gmail.com>
2023-06-26 15:51:47 +0200

    language fix

--- inc/pgs/product_upload.php
+++ inc/pgs/product_upload.php
@@ -30,7 +30,7 @@
     <div class="row">
       <div class="col">
         <div class="container mx-auto m-4">
-          <h1 class="mb-2">Produkt Upload</h1>
+          <h1 class="mb-2">Product Upload</h1>
           <form method="post" action="product_upload.php" enctype="multipart/form-data">
             <div class="mb-3">
               <label for="name" class="form-label">Product Name:</label>
@@ -44,22 +44,22 @@
               <label for="price" class="form-label">Price:</label>
               <input type="number" name="price" step="0.01" class="form-control" required>
             </div>
-            <?php if($role == "administrator")
-            echo "
+            <?php if ($role == "administrator")
+              echo "
             <div class='mb-3'>
               <label for='sellerid' class='form-label'>Seller ID:</label>
               <input type='number' name='sellerid' step='0.01' class='form-control' required>
             </div>
             " ?>
-            <div class="mb-3">
-              <label for="category_id" class="form-label">Category:</label>
-              <select name="category_id" class="form-select" required>
-                <option value="">-- Select a Category --</option>
-                <?php
-                foreach ($categories as $category) {
-                  echo '<option value="' . $category['id'] . '">' . $category['name'] . '</option>';
-                }
-                ?>
+              <div class="mb-3">
+                <label for="category_id" class="form-label">Category:</label>
+                <select name="category_id" class="form-select" required>
+                  <option value="">-- Select a Category --</option>
+                  <?php
+            foreach ($categories as $category) {
+              echo '<option value="' . $category['id'] . '">' . $category['name'] . '</option>';
+            }
+            ?>
               </select>
             </div>
             <div class="mb-3">
@@ -71,55 +71,55 @@
         </div>
       </div>
     </div>
- 
-    
 
-  
+
+
+
+
+    <?php
+    // Process the form data and insert the product into the database
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+      $role == "administrator" ? $sellerId = $_POST['sellerid'] : $sellerId = $currentUser['seller_id'];
+
+      $name = $_POST['name'];
+      $description = $_POST['description'];
+      $price = $_POST['price'];
+      $category_id = $_POST['category_id'];
+
+      if (isset($_FILES['image']) && $_FILES['image']['error'] == UPLOAD_ERR_OK) {
+        $imagePath = './../../res/img/' . uniqid() . '_' . basename($_FILES['image']['name']);
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $imagePath)) {
+          $sql = 'INSERT INTO products (seller_id, category_id, name, description, price) VALUES (?, ?, ?, ?, ?)';
+          $stmt = $db_obj->prepare($sql);
+          $stmt->bind_param('iissd', $sellerId, $category_id, $name, $description, $price);
+          $stmt->execute();
+
+          $productId = $db_obj->insert_id;
+
+          $sql = 'INSERT INTO product_images (product_id, image_path) VALUES (?, ?)';
+          $stmt = $db_obj->prepare($sql);
+          $stmt->bind_param('is', $productId, $imagePath);
+          $stmt->execute();
+
+          echo '<p>Product uploaded successfully.</p>';
+        } else {
+          echo '<p>There was an error uploading the product image.</p>';
+        }
+      } else {
+        echo '<p>Please select a product image to upload.</p>';
+      }
+    }
+
+    // Close the database connection
+    $db_obj->close();
+    ?>
+
+  </div>
 
   <?php
-  // Process the form data and insert the product into the database
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $role == "administrator"? $sellerId = $_POST['sellerid']:  $sellerId = $currentUser['seller_id'];
-    
-    $name = $_POST['name'];
-    $description = $_POST['description'];
-    $price = $_POST['price'];
-    $category_id = $_POST['category_id'];
-
-    if (isset($_FILES['image']) && $_FILES['image']['error'] == UPLOAD_ERR_OK) {
-      $imagePath = './../../res/img/' . uniqid() . '_' . basename($_FILES['image']['name']);
-      if (move_uploaded_file($_FILES['image']['tmp_name'], $imagePath)) {
-        $sql = 'INSERT INTO products (seller_id, category_id, name, description, price) VALUES (?, ?, ?, ?, ?)';
-        $stmt = $db_obj->prepare($sql);
-        $stmt->bind_param('iissd', $sellerId, $category_id, $name, $description, $price);
-        $stmt->execute();
-
-        $productId = $db_obj->insert_id;
-
-        $sql = 'INSERT INTO product_images (product_id, image_path) VALUES (?, ?)';
-        $stmt = $db_obj->prepare($sql);
-        $stmt->bind_param('is', $productId, $imagePath);
-        $stmt->execute();
-
-        echo '<p>Product uploaded successfully.</p>';
-      } else {
-        echo '<p>There was an error uploading the product image.</p>';
-      }
-    } else {
-      echo '<p>Please select a product image to upload.</p>';
-    }
-  }
-
-  // Close the database connection
-  $db_obj->close();
+  include '../includes/footer.php';
   ?>
-
- </div>
-
-<?php
-        include '../includes/footer.php';
-        ?>
 </body>
 
 </html>
